8d6342cfb36a3d914436892de66cd6cd4bf8dd49
Vjaceslavs Jermakovs <vjacheslav.jermakov@gmail.com>
2023-05-19 14:48:22 +0300

    Some additions

--- src/test/java/features/Login.feature
+++ src/test/java/features/Login.feature
@@ -7,6 +7,12 @@ Feature: Application login
 
   Scenario: Home page default login
     Given user is on NetBanking landing page
-    When user login into application with username and password
+    When user login into application with "jin" username and password "1234"
     Then home page is populated
-    And cards are displayed
+    And cards are displayed are "true"
+
+  Scenario: Home page default login
+    Given user is on NetBanking landing page
+    When user login into application with "john" username and password "4321"
+    Then home page is populated
+    And cards are displayed are "false"
--- src/test/java/stepDefinitions/StepDefinition.java
+++ src/test/java/stepDefinitions/StepDefinition.java
@@ -4,26 +4,32 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+
 public class StepDefinition {
 
     @Given("^user is on NetBanking landing page$")
     public void user_is_on_netbanking_landing_page() throws Throwable {
         //code to navigate to login url
+        System.out.println("navigated to login url");
     }
 
-    @When("^user login into application with username and password$")
-    public void user_login_into_application_with_username_and_password() throws Throwable {
+    @When("^user login into application with \"([^\"]*)\" username and password \"([^\"]*)\"$")
+    public void user_login_into_application_with_username_and_password(String strArg1, String strArg2) throws Throwable {
         //code to log in
+        System.out.println(strArg1);
+        System.out.println(strArg2);
     }
 
     @Then("^home page is populated$")
     public void home_page_is_populated() throws Throwable {
         //home page validation
+        System.out.println("validate home page");
     }
 
-    @And("^cards are displayed$")
-    public void cards_are_displayed() throws Throwable {
+    @And("^cards are displayed are \"([^\"]*)\"$")
+    public void cards_are_displayed(String arg1) throws Throwable {
         //validating the cards
+        System.out.println(arg1);
     }
 
 }
